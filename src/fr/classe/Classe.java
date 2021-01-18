eaa4c1b93ce130b68ae1fa65eb100199e17304bd
frenchincubus <lionelrenier82@gmail.com>
2021-01-18 14:38:19 +0100

    #21 - 7.1 création classe Classe

---
+++ src/fr/classe/Classe.java
@@ -0,0 +1,40 @@
+package fr.classe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.personnage.IAttaque;
+
+public class Classe {
+	
+	public String nom;
+	public List<IAttaque> attaques = new ArrayList<>();
+
+	public Classe(String nom, List<IAttaque> attaques) {
+       this.nom = nom;
+       this.attaques = attaques;
+    }
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public List<IAttaque> getAttaques() {
+		return attaques;
+	}
+
+	public void setAttaques(List<IAttaque> attaques) {
+		this.attaques = attaques;
+	}
+
+	@Override
+	public String toString() {
+		return "Classe [nom=" + nom + ", attaques=" + attaques + "]";
+	}
+	
+	
+}
