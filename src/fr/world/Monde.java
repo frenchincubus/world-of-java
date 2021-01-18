e132b0845d6255e09f203e069f04fa395ed1661e
frenchincubus <lionelrenier82@gmail.com>
2021-01-18 11:18:20 +0100

    #6 - 2.2 PersonnageFactory déplacé dans classe Monde

--- src/fr/world/Monde.java
+++ src/fr/world/Monde.java
@@ -1,5 +1,25 @@
 package fr.world;
 
+import java.util.Scanner;
+
+import fr.personnage.Personnage;
+
 public class Monde {
 
+	/**
+	* Créer un personnage avec touss ses attributs. 
+	* Demande a l'utilisateur d'entrer le nom du personnage.
+	* retour: une instance de la classe Personnage correctement instancié.
+	**/
+	public static Personnage personnageFactory() {
+		// Demander a l'utilisateur un nom de personnage
+	    // Creer un nouveau personnage en utilisant le constructeur avec tous ses params (dont le nom qui vient d'être choisi par l'utilisateur)
+	    // Retourner l'instance du personnage
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez entrer le nom de votre personnage: ");
+		String nom = sc.nextLine();		
+		sc.close();
+		
+		return new Personnage(50, 15, nom);
+	}
 }
