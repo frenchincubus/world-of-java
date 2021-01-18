a583ef520ef3ccff869076f58d7bd852e555f40f
frenchincubus <lionelrenier82@gmail.com>
2021-01-18 12:29:00 +0100

    #16 - 5.1 création de l'interface ICombattant

---
+++ src/fr/personnage/ICombattant.java
@@ -0,0 +1,23 @@
+package fr.personnage;
+
+public interface ICombattant {
+	/**
+	 * Le combattant attaque un adversaire
+	 * @param ICombattant adversaire : un adversaire de type ICombattant
+	 */
+	public void attaquer(ICombattant adversaire);
+	
+	/**
+	 * Le combattant absorbe des dégats reçus
+	 * @param int degats : les dégats infligés au combattant
+	 */
+    public void defendre(int degats);
+
+    // Getters et Setters
+    public String getNom();
+    public int getDegats();
+    public int getPointDeVie();
+    public void setNom(String nom);
+    public void setDegats(int degats);
+    public void setPointDeVie(int pointDeVie);
+}
